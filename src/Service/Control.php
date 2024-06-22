b9d68b95bd00482cbf391e579a2d4f16dd6e9616
Daniel Bispo <szagot@gmail.com>
2024-06-21 22:57:32 -0300

    Limpeza

--- src/Service/Control.php
+++ src/Service/Control.php
@@ -3,9 +3,6 @@
 namespace Ancient\Service;
 
 use Ancient\Config\Database;
-use Ancient\Models\Person;
-use Ancient\Models\Question;
-use JetBrains\PhpStorm\NoReturn;
 use Sz\Config\Uri;
 
 class Control
